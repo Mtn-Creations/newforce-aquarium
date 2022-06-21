b5d05cf2d06c948f9e8a426d53578403ca108a57
Jason Ellison <jason.w.ellison@gmail.com>
2022-06-21 13:47:55 -0400

    Added scripts/database.js

---
+++ scripts/database.js
@@ -0,0 +1,65 @@
+const database = {
+    fish: [
+        {
+            species: "Moorish Idol",
+            size: 6,
+            name: "Edward",
+            location: "Maui",
+            diet: [
+                "Sponges",
+                "Coral polyps",
+                "Tunicates",
+                "Misc. Benthic invertebrates"
+            ]
+        },
+        {
+            species: "Saddle Wrasse",
+            size: 4,
+            name: "Franklin",
+            location: "Maui",
+            diet: ["Worms", "Shells", "Mollusks", "Shrimp"]
+        },
+        {
+            species: "Yellow Tang",
+            size: 4,
+            name: "Susan",
+            location: "Maui",
+            diet: ["Benthic turf algae", "Misc. marine plant materia"]
+        },
+        {
+            species: "Sergent Major",
+            size: 4,
+            name: "Dave",
+            location: "Bermuda",
+            diet: ["Larvae of invertebrates", "Zooplankton", "Smaller fish", "Crustaceans", "Various species of algae"]
+        },
+        {
+            species: "Rock Beauty",
+            size: 5,
+            name: "Rocky",
+            location: "Bermuda",
+            diet: ["Benthic invertebrates", "Mucus secreted by other fish"]
+        },
+        {
+            species: "Squirrelfish",
+            size: 3,
+            name: "Sam",
+            location: "Bermuda",
+            diet: ["Benthic invertebrates"]
+        },
+        {
+            species: "Damselfish",
+            size: 3,
+            name: "Amanda",
+            location: "Aculpulco",
+            diet: ["Plankton", "Caridea", "Copepods"]
+        },
+        {
+            species: "Finescale Triggerfish",
+            size: 6,
+            name: "Bob",
+            location: "Bermuda",
+            diet: ["Bottom dwelling crustaceans", "Mollusks", "Sea urchins", "Misc. echinoderms"]
+        },
+    ]
+}
